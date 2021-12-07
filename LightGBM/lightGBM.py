f478e8c4ca0f0f5374583269b1a83b710e972833
alex-de-zwart-priv <alex.dezwart@outlook.com>
2021-12-07 20:15:11 +0100

    lightgbmstatus

--- LightGBM/lightGBM.py
+++ LightGBM/lightGBM.py
@@ -1,16 +1,16 @@
 import lightgbm as lgb
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import pandas as pd
+import os
 
 path = os.getcwd()
 
 #LOAD DATA
-pd.read_csv(os.path.join(path,'x_train.csv'), sep=';')
-pd.read_csv(os.path.join(path,'y_train.csv'), sep=';')
-pd.read_csv(os.path.join(path,'x_val.csv'), sep=';')
-pd.read_csv(os.path.join(path,'y_val.csv'), sep=';')
+x_train = pd.read_csv(os.path.join(path,'x_train.csv'), sep=';')
+y_train = pd.read_csv(os.path.join(path,'y_train.csv'), sep=';')
+x_val = pd.read_csv(os.path.join(path,'x_val.csv'), sep=';')
+y_val = pd.read_csv(os.path.join(path,'y_val.csv'), sep=';')
 
 
 
@@ -29,21 +29,36 @@ features = [
  'event_type_2',
  'snap_CA']
 
-params = {
-    'num_leaves': 555,
-    'min_child_weight': 0.034,
-    'feature_fraction': 0.379,
-    'bagging_fraction': 0.418,
-    'min_data_in_leaf': 106,
-    'boosting_type': 'gbdt',
-    'metric': 'rmse',
-    'objective': 'regression',
-    'n_jobs': -1,
-    'seed': 236,
-    'learning_rate': 0.01,
-    'bagging_fraction': 0.75,
-    'bagging_freq': 10,
-    'colsample_bytree': 0.75}
+# params = {
+#     'num_leaves': 555,
+#     # 'min_child_weight': 0.034,
+#     # 'feature_fraction': 0.379,
+#     # 'bagging_fraction': 0.418,
+#     # 'min_data_in_leaf': 106,
+#     'boosting_type': 'gbdt',
+#     'metric': 'rmse',
+#     'force_row_wise':True,
+#     'objective': 'poisson',
+#     'n_jobs': -1,
+#     'seed': 236,
+#     'learning_rate': 0.01,
+#     'bagging_fraction': 0.75,
+#     'bagging_freq': 10,
+#     'colsample_bytree': 0.75}
+
+params={
+    'objective':'poisson',
+    'metric':['rmse'],
+    'force_row_wise':True,
+    'learning_rate':0.075,
+    'sub_row': 0.75,
+    'bagging_freq': 1,
+    'lambda_12':0.1,
+    'verbosity':1,
+    'num_iterations':1200,
+    'num_leaves':2**6 -1,
+    'min_data_in_leaf':2**6 -1
+}
 
 
 train_lgbm = lgb.Dataset(x_train[features], y_train)
@@ -54,7 +69,7 @@ from sklearn import metrics
 model = lgb.train(params,
                    train_lgbm,
                    num_boost_round = 100000,
-                   early_stopping_rounds = 1000,
+                   early_stopping_rounds = 10000,
                    valid_sets = [train_lgbm, val_lgbm],
                    verbose_eval = 100)
 
